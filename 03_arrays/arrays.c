1d607128283209a143e50594d2410913eedc3eaf
salahudin001 <salahudinadan2069@gmail.com>
2023-01-23 14:37:28 +0300

    added arrays

---
+++ 03_arrays/arrays.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+
+// void main()
+// {
+
+//     int score[] = {50, 60, 70, 80, 7, 42, 13, 76, 54, 87, 32, 95, 37, 68, 59, 67, 98, 90};
+//     for (int k = 0; k < 17; k++)
+//     {
+//         printf("This is posion %i %i\n", k, score[k]);
+//         if (score[k] >= 70)
+//         {
+//             printf("pass\n");
+//         }
+//         else
+//         {
+//             printf("fail\n");
+//         }
+//     }
+// }
+// void main()
+// {
+//     int score[] = {50, 72, 39, 47, 82, 35, 73, 28, 73, 50, 57, 33, 87, 53, 02, 33, 54, 65, 34};
+//     int pass = 0, fail = 0;
+
+//     for (int k = 0; k < 18; k++)
+//     {
+//         printf("This is posion %i %i\n", k, score[k]);
+//         if (score[k] >= 70)
+//         {
+//             printf("pass\n");
+//             pass = pass + 1;
+//         }
+//         else
+//         {
+//             printf("fail\n");
+//             fail = fail + 1;
+//             printf("\n");                      
+//         }
+//         printf("pass %i\n", pass);
+//         printf("fail %i\n", fail);
+
+//         int score[] = {97, 46, 9, 28, 37, 04, 76, 51, 75, 84, 57, 84, 29, 29, 5, 78, 02, 56, 73, 4, 92, 3};
+//         int pass = 0, fail = 0;
+
+//         for (int k = 0; k < 18; k++)
+//         {
+//             if (score[k] >= 65)
+//             {
+//                 printf("pass\n");
+//                 pass = pass + 1;
+//             }
+//             else
+//             {
+//                 printf("fail\n");
+//                 fail = fail + 1;
+//                 printf("\n");
+//             }
+//             printf("pass %i\n", pass);
+//             printf("fail %i\n", fail);
+//         }
+//         printf("After the loop \n");
+//         printf("pass %i\n", pass);
+//         printf("fail %i\n", fail);
+//     }
+// }
+
+// void main()
+// {
+
+//     int score[] = {76, 92, 83, 80, 40, 35, 37, 66, 58, 43, 56, 96, 65, 64, 47, 89};
+//     int pass = 0, fail = 0;
+//     for (int x = 0; x <= 16; x++)
+//     {
+//         printf("This is posion %i,%i\n", x, score[x]);
+//         if (score[x] >= 60)
+//         {
+//             printf("pass\n");
+//             pass = pass + 1;
+//         }
+//         else
+//         {
+//             printf("fail\n");
+//             fail = fail + 1;
+//         }
+//     }
+//     printf("After the loop \n");
+//     printf("pass %i\n", pass);
+//     printf("fail %i\n", fail);
+// }
+
