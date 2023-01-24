677592d3fb01b52a0e3d94ba6c310f4634b11ede
salahudin001 <salahudinadan2069@gmail.com>
2023-01-24 14:52:46 +0300

    commit

---
+++ 05_div/div.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+void main () {
+  // double d1,d2,div;
+  // printf("Enter num1\n");
+ //  scanf("%lf",&d1);
+ //  printf("Enter num2\n");
+ //  scanf("%lf",&d2);
+ //  div = d1/ d2;
+  // printf("the division is %lf\n",div);
+ //double d1,d2,div ;
+ //printf("Enter first number ");
+ //scanf("%lf",&d1);
+ //printf("Enter second number ");
+ //scanf("%lf",&d2);
+// div = d1/ d2;
+ //printf("the division is %lf\n",div);
+ //double d1 , d2 , div ;
+ //printf("Enter first number\n");
+ //scanf("%lf",&d1);
+ //printf("Enter second number\n");
+ //scanf("%lf",&d2);
+ //div = d1/d2;
+ //printf("The divesion is %lf\n",div);
+ //double d1 , d2 ,div;
+//printf("Enter the first number");
+ //scanf("%lf",&d1);
+ //printf("Enter the second number");
+ //scanf("%lf",&d2);
+ //div = d1/ d2 ;
+ //printf("The division is %lf\n",div);
+ //double div , num1 , num2 ;
+ //printf(" Enter the first number\n");
+ //scanf("%lf",&num1);
+ //printf("Enter second number\n");
+ //scanf("%lf",&num2);
+ //div = num1 / num2 ;
+ //printf("The division is %lf\n",div);
+
+ printf("Salahudin adam\n");
+ double div , num1 , num2 ;
+}
